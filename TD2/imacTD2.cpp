b878fdce34795f5ba4c7a741f9b7c1c0ef21b9b4
PaulMarchiset <paul.marchiset@edu.univ-fcomte.fr>
2024-09-17 15:20:58 +0200

    TD3 exercice 2

--- TD2/imacTD2.cpp
+++ TD2/imacTD2.cpp
@@ -9,3 +9,31 @@
 //     std::cout << "Le nombre " << a << " est " << verif << std::endl;
 // }
 
+// **EXERCICE 2**
+int main()
+{
+
+    int number;
+    int somme{0};
+    float compteur{0};
+
+    do
+    {
+        std::cout << "Entre un nombre: ";
+
+        std::cin >> number;
+        if (number < 0) {
+            break;
+        };
+
+        somme += number;
+
+        compteur+=1;
+
+    } while (number > 0);
+
+    std::cout << "La somme vaut : " << somme << std::endl;
+    std::cout << "La moyenne vaut : " << somme / compteur << std::endl;
+
+    return 0;
+}
