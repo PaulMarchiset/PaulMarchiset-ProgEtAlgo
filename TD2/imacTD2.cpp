a60f1c312e6717cc614f2fb3d90f4ef5e37a3fd4
PaulMarchiset <paul.marchiset@edu.univ-fcomte.fr>
2024-09-17 15:36:39 +0200

    TD3 Exercice 3

--- TD2/imacTD2.cpp
+++ TD2/imacTD2.cpp
@@ -10,30 +10,55 @@
 // }
 
 // **EXERCICE 2**
-int main()
-{
+// int main()
+// {
 
-    int number;
-    int somme{0};
-    float compteur{0};
+//     int number;
+//     int somme{0};
+//     float compteur{0};
 
-    do
-    {
-        std::cout << "Entre un nombre: ";
+//     while (true) 
+//     {
+//         std::cout << "Entre un nombre: ";
 
-        std::cin >> number;
-        if (number < 0) {
+//         std::cin >> number;
+//         if (number < 0) {
+//             break;
+//         };
+
+//         somme += number;
+
+//         compteur+=1;
+
+//     };
+
+//     std::cout << "La somme vaut : " << somme << std::endl;
+//     std::cout << "La moyenne vaut : " << somme / compteur << std::endl;
+
+//     return 0;
+// }
+
+// **EXERCICE 3**
+int main() {
+    int number {0};
+    std::cout << "Entrez un nombre entier : ";
+    std::cin >> number;
+
+    int verif {number % 2};
+
+    std::string pair {(verif == 0) ? "pair" : "impair" };
+
+    std::cout << number << " est " << pair << std::endl;
+
+    int pairOuImpair {number};
+
+    while (true) {
+        pairOuImpair = pairOuImpair - 2;
+        if (pairOuImpair < 0) {
             break;
-        };
-
-        somme += number;
-
-        compteur+=1;
-
-    } while (number > 0);
-
-    std::cout << "La somme vaut : " << somme << std::endl;
-    std::cout << "La moyenne vaut : " << somme / compteur << std::endl;
+        }
+        std::cout << pairOuImpair << std::endl;
+    }
 
     return 0;
 }
