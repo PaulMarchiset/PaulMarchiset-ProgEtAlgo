0b764a246f8fc421301f32562d2f435f26024990
PaulMarchiset <paul.marchiset@edu.univ-fcomte.fr>
2024-09-17 13:56:57 +0200

    first commit

---
+++ TD1/helloImac.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+// **EXERCICE 1**
+// int main()
+// {
+//     int number { 1 };
+//     number++;
+//     number*=3;
+//     number/=2;
+//     int result {number};
+//     std::cout << result << std::endl;
+//     return 0;
+// }
+
+
+// **EXERCICE 2**
+// int main()
+// {
+//     std::cout << "Entre 3 nombres : " << std::endl;
+//     float a {0};
+//     std::cin >> a;
+//     float b {0};
+//     std::cin >> b;
+//     float c {0};
+//     std::cin >> c;
+//     float somme {a + b + c};
+//     float moyenne {somme / 3};
+//     std::cout << "La somme est : " << somme << std::endl;
+//     std::cout << "La moyenne est : " << moyenne << std::endl;
+//     return 0;
+// }
+
+// **EXERCICE 3**
+// int main() {
+//     int number {11};
+//     std::cout << "la valeur est : " << number << std::endl;
+//     float result {static_cast<float>(number)};
+//     result /=2;
+//     std::cout << "la valeur est : " << result << std::endl;
+//     return 0;
+// }
+
+// **EXERCICE 4**
+int main() {
+    int joursUser {0};
+    std::cout << "Entrez un nombre de jours : " << std::endl;
+    std::cin >> joursUser;
+    int annees {joursUser / 365};
+    int resteAnnees {joursUser % 365};
+    int mois {resteAnnees / 30};
+    int resteMois {resteAnnees % 30};
+    int semaines {resteMois / 7};
+    int jours {resteMois % 7};
+    std::cout << "Il y a " << annees << " annees, " << mois << " mois, " << semaines << " semaines et " << jours << " jours." << std::endl;
+}
---
+++ build/CMakeFiles/helloImac.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloImac.dir/helloImac.cpp.obj"
+  "CMakeFiles/helloImac.dir/helloImac.cpp.obj.d"
+  "helloImac.exe"
+  "helloImac.exe.manifest"
+  "helloImac.pdb"
+  "libhelloImac.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloImac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
